fafa0042259ff71674e321b3e120bda0155b9304
Rafael Azzolini <rafaazzolini@hotmail.com>
2025-08-15 23:27:49 -0300

    Initial struture

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ mensagem: "API funcionando" });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
